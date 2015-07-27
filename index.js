8146d2dba38845f5573b01023e4e54f45e5acc60
pemrouz <p3pedram@hotmail.com>
2015-07-27 07:18:54 +0100

    move to npm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var is = require('is')
-  , not = require('not')
-  , keys = require('keys')
-  , copy = require('copy')
+var is = require('utilise.is')
+  , not = require('utilise.not')
+  , keys = require('utilise.keys')
+  , copy = require('utilise.copy')
 
 module.exports = function extend(to){ 
   return function(from){
